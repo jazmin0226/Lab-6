90323993c0a16cf68f3c55a1e8c6432fc5bccbb7
jazmin0226 <jariass@ucenfotec.ac.cr>
2020-11-07 20:32:02 -0600

    Initial commit

---
+++ journal/src/main/java/com/cenfotec/journal/repository/JournalRepository.java
@@ -0,0 +1,7 @@
+package com.cenfotec.journal.repository;
+
+import com.cenfotec.journal.domain.Journal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JournalRepository extends JpaRepository<Journal, Long> {
+}
